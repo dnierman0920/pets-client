f0e073d29e3efb27d5e0729667dd43f9b2dceb5c
dnierman0920 <davidnierman93@gmail.com>
2022-03-31 09:02:25 -0700

    Add pet SHOW & CREATE routes

--- src/api/pets.js
+++ src/api/pets.js
@@ -5,3 +5,24 @@ import axios from 'axios'
 export const getAllPets = () => {
     return axios(`${apiUrl}/pets`)
 }
+
+// show function
+export const getOnePet = (petId) => {
+    return axios(`${apiUrl}/pets/${petId}`)
+}
+
+// POST -> create function
+export const createPet = (user, newPet) => {
+    return axios({
+        url: `${apiUrl}/pets`,
+        method: 'POST',
+        headers: {
+            Authorization: `Token token=${user.token}`
+        },
+        data: newPet
+    })
+}
+
+// PATCH -> update function
+
+// DELETE -> remove function
